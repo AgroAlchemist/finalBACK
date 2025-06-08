adc405b1db7cabb2e582217b960ad2c4adfb353c
Omkar Kulkarni <o.s.kulkarni2004@gmail.com>
2025-06-09 00:17:56 +0530

    add landing page

--- database.js
+++ database.js
@@ -2,11 +2,10 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
 const pool = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-
+    host: process.env.HOST  ,
+    user: process.env.USER  ,
+    password: process.env.PASSWORD ,
+    database: process.env.DATABASE 
 }).promise();
 
 
@@ -16,9 +15,10 @@ export  async function showTables() {
 }
 
 export  async function showUsers(){
-    const result = await pool.query('select username from farmers')
+    const result = await pool.query('select * from farmers')
     return result[0];
 }
+
 
 //showTables().then((result) => console.log(result))
 export default {pool,showTables, showUsers}
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import cropRoutes from './routes/crops.js';
 import userRoutes from './routes/users.js';
 import rentalRoutes from './routes/rental.js';
 import { showTables } from './database.js';
+import path from "path";
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -17,6 +19,18 @@ app.use('/farmer', farmerRoutes);
 app.use('/crops', cropRoutes);
 app.use('/users', userRoutes);
 app.use('/rental', rentalRoutes);
+// app.get('/', (req, res) => {
+//     res.sendFile('C:/Users/oskul/Downloads/webdevops/befe/backend/landing.html', (er)=>{
+//         console.log("Could not load landing page");
+//     })
+// })
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Serve the HTML file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "landing.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
